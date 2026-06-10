0ff56a0f2acbff11b69121920e33922f8df2b51b
Pratik Savant <savantpratik823@gmail.com>
2026-06-10 21:40:01 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    printf("Enter first numbers: ");
+    printf("Enter second numbers: ");
+    scanf("%d %d", &a, &b);
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
